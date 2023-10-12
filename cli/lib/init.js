ae222a59c4fbf960f44c2ce83cc521829f89b9a1
Marak <marak.squires@gmail.com>
2023-10-12 16:18:15 -0400

    Removes use of `require.resolve` for package location

--- cli/lib/init.js
+++ cli/lib/init.js
@@ -49,8 +49,11 @@ init.configureWorld = async function configureWorld(sourceDir, defaultWorldName)
 }
 
 init.scaffold = async function scaffoldInit() {
-  const sourceDir = path.dirname(require.resolve('@yantra-core/examples')) + '/world';
+
+  const sourceDir = path.resolve(process.cwd(), 'node_modules/@yantra-core/examples/world');
+
   const defaultWorldName = path.basename(process.cwd());
+
   await init.configureWorld(sourceDir, defaultWorldName);
 }
 
@@ -70,7 +73,8 @@ init.clone = async function cloneInit(worldname) {
     return;
   }
 
-  const sourceDir = path.dirname(require.resolve('@yantra-core/examples')) + '/' + worldname;
+  const sourceDir = path.resolve(process.cwd(), 'node_modules/@yantra-core/examples', worldname);
+
   await init.configureWorld(sourceDir, worldname);
 }
 
