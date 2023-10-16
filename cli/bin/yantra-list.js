7d399d13dfa31c177a30b66696f11ac103fbb361
Marak <marak.squires@gmail.com>
2023-10-16 13:42:28 -0400

    Adds world links to `yantra list`

--- cli/bin/yantra-list.js
+++ cli/bin/yantra-list.js
@@ -3,6 +3,8 @@ import yantra from '@yantra-core/client';
 import { existsSync, readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import minimist from 'minimist';
+let argv = minimist(process.argv.slice(2));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -25,7 +27,22 @@ async function go() {
     console.log('Run `yantra clone` to copy an existing Yantra world.');
 
   } else {
-    console.log('Available Worlds:', worlds);
+
+    if (argv.json || argv.j) {
+      console.log(JSON.stringify(worlds, null, 2));
+      return;
+    }
+
+    console.log('Available Worlds:');
+    worlds.forEach(function(world) {
+      let gameLink = `https://ayyo.gg/play?mode=${world.mode}&owner=${world.owner}`;
+  
+      console.log(JSON.stringify(world, null, 2));
+      console.log('');
+      console.log(gameLink);
+      
+
+    })
   }
 }
 
